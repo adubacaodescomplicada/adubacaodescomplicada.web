dc06589d05fa894ce6b05fa1779f421b3a0486e1
Adubação Descomplicada <adubacaodescomplicada@gmail.com>
2020-08-06 22:00:19 -0300

    ajuste
    Signed-off-by: Adubação Descomplicada <adubacaodescomplicada@gmail.com>

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   ambiente: 'produção',
-  REST_API_URL: 'https://adubacaodescomplicada-rest.herokuapp.com/',
+  REST_API_URL: 'https://adubacaodescomplicada-rest.herokuapp.com',
   AUTHORIZATION_SERVER: 'https://adubacaodescomplicada-rest.herokuapp.com',
   CLIENT_ID: 'adubacaodescomplicada',
   CLIENT_SECRET: 'adubacaodescomplicada',
